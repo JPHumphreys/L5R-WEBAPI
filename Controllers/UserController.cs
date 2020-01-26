be7b226e16d8a923f09df15082105999db2743b2
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-01-26 19:52:07 +0000

    created a post for users that connects with the database

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -41,15 +41,52 @@ namespace L5R_API.Controllers
             return users;
         }
 
-        // GET: api/User/5
-        public string Get(int id)
+        // GET: api/User/cardname
+        public List<User> Get(string id)
         {
-            return "value";
+            _con = new SqlConnection("Server= localhost; Database=l5r; Integrated Security=True;");
+            DataTable _dt = new DataTable();
+            var query = "SELECT * FROM Users WHERE username=" + id;
+            _adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand(query, _con)
+            };
+            _adapter.Fill(_dt);
+            List<User> users = new List<Models.User>(_dt.Rows.Count);
+
+            if (_dt.Rows.Count > 0)
+            {
+                foreach (DataRow userRecord in _dt.Rows)
+                {
+                    users.Add(new ReadUser(userRecord));
+                }
+            }
+
+            return users;
         }
 
         // POST: api/User
-        public void Post([FromBody]string value)
+        public string Post([FromBody]createUser value)
         {
+
+            _con = new SqlConnection("Server= localhost; Database=l5r; Integrated Security=True;");
+            var query = "INSERT INTO Users (username, password) VALUES(@username, @password)";
+            SqlCommand insertCommand = new SqlCommand(query, _con);
+
+            insertCommand.Parameters.AddWithValue("@username", value.username);
+            insertCommand.Parameters.AddWithValue("@password", value.password);
+
+            _con.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if(result > 0)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+
         }
 
         // PUT: api/User/5
