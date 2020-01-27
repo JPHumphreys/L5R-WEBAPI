2055e493108e63f20263b19ec0d8cd86fd751843
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-01-27 12:11:06 +0000

    created the headings for the card model

--- Models/Card.cs
+++ Models/Card.cs
@@ -10,23 +10,48 @@ namespace L5R_API.Models
     /// </summary>
     public class Card
     {
-        /// <summary>
-        /// Card ID in a form of "cardfirstname-cardothernames-n-n-n"
-        /// Eg. "shrewd-yasuki", "yorimoto", "hiru-mori-toride-unicorn"
-        /// </summary>
         public string id { get; set; } = "";
-        /// <summary>
-        /// This is the card clan affiliation
-        /// </summary>
+
         public string clan { get; set; } = "";
-        /// <summary>
-        /// This is the deck side in which the card is legally played
-        /// </summary>
+
+        public int cost { get; set; } = 0;
+
+        public string element { get; set; } = "";
+
+        public int fate { get; set; } = 0;
+
+        public int glory { get; set; } = 0;
+
+        public int honor { get; set; } = 0;
+
+        public string imglocation { get; set; } = "";
+
+        public int influencecost { get; set; } = 0;
+
+        public int influencepool { get; set; } = 0;
+
+        public string isrestricted { get; set; } = "";
+
+        // This is a TRUE/FALSE indicator to tell whether the card is banned or not (not yet implemented)
+        //public string isbanned { get; set; } = "";
+
+        public int military { get; set; } = 0;
+
+        public string militarybonus { get; set; } = "";
+
+        public string rolerestriction { get; set; } = "";
+
         public string side { get; set; } = "";
-        /// <summary>
-        /// This is the legal card type
-        /// Eg. "event","attachment"
-        /// </summary>
+
+        public int strength { get; set; } = 0;
+
+        public string strengthbonus { get; set; } = "";
+
+        public string text { get; set; } = "";
+
         public string type { get; set; } = "";
+
+        public string unicity { get; set; } = "";
+
     }
 }
