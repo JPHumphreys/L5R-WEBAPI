fb9299eba9b0cdc6ac6192a4217fdab8f7fe0c72
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-01-27 19:34:24 +0000

    added summaries for the ratings model

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -8,24 +8,73 @@ namespace L5R_API.Models
 {
     public class Rating
     {
+        /// <summary>
+        /// This is the id that is shared with card id's - this is a foreign key
+        /// </summary>
         public string id { get; set; } = "";
-        public string clan { get; set; } = "";
 
+        /// <summary>
+        /// this a calculation:
+        /// (rating + votes) + n + n / total of all votes
+        /// </summary>
         public float overallrating { get; set; } = 0.0f;
+
+        /// <summary>
+        /// The rating for the crab matchup
+        /// </summary>
         public float ratingcrab { get; set; } = 0.0f;
+        /// <summary>
+        /// The rating for the crane matchup
+        /// </summary>
         public float ratingcrane { get; set; } = 0.0f;
+        /// <summary>
+        /// The rating for the dragon matchup
+        /// </summary>
         public float ratingdragon { get; set; } = 0.0f;
+        /// <summary>
+        /// The rating for the lion matchup
+        /// </summary>
         public float ratinglion { get; set; } = 0.0f;
+        /// <summary>
+        /// The rating for the phoenix matchup
+        /// </summary>
         public float ratingphoenix { get; set; } = 0.0f;
+        /// <summary>
+        /// The rating for the scorpion matchup
+        /// </summary>
         public float ratingscorpion { get; set; } = 0.0f;
+        /// <summary>
+        /// The rating for the unicorn matchup
+        /// </summary>
         public float ratingunicorn { get; set; } = 0.0f;
 
+        /// <summary>
+        /// The total votes for all crab votes
+        /// </summary>
         public int totalvotescrab { get; set; } = 0;
+        /// <summary>
+        /// The total votes for all crane votes
+        /// </summary>
         public int totalvotescrane { get; set; } = 0;
+        /// <summary>
+        /// the total votes for all dragon votes
+        /// </summary>
         public int totalvotesdragon { get; set; } = 0;
+        /// <summary>
+        /// the total votes for all lion votes
+        /// </summary>
         public int totalvoteslion { get; set; } = 0;
+        /// <summary>
+        /// the total votes for all phoenix votes
+        /// </summary>
         public int totalvotesphoenix { get; set; } = 0;
+        /// <summary>
+        /// the total votes for all scorpion votes
+        /// </summary>
         public int totalvotesscorpion { get; set; } = 0;
+        /// <summary>
+        /// the total votes for all unicron votes
+        /// </summary>
         public int totalvotesunicorn { get; set; } = 0;
     }
 
@@ -39,7 +88,6 @@ namespace L5R_API.Models
         public ReadRating(DataRow row)
         {
             id = row["id"].ToString();
-            clan = row["clan"].ToString();
 
             overallrating = Convert.ToSingle(row["overallrating"]);
             ratingcrab = Convert.ToSingle(row["ratingcrab"]);
